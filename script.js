415205fe2bccc6c487d0eeefa1a95904b950f3b2
CiroTol <cirotoledo02@gmail.com>
2025-07-31 16:38:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let blanco = true;
+
+function oscuro() {
+    document.body.style.backgroundColor = blanco ? 'black' : '#FFF5E1';
+    document.body.style.color = blanco ? 'black' : '#A67C52';
+    blanco = !blanco;
+}
+
+
+function copyright() {
+alert ("Desarrollado por: Ciro Toledo --- Mail:cirotoledo2009@hotmail.com --- Todos los derechos reservados. ©"
+)
+}
