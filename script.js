2e1084d1d329c6251f8a42e026f16151c8d0bded
CiroTol <cirotoledo02@gmail.com>
2025-07-31 16:39:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let blanco = true;
 
 function oscuro() {
-    document.body.style.backgroundColor = blanco ? 'black' : '#FFF5E1';
-    document.body.style.color = blanco ? 'black' : '#A67C52';
-    blanco = !blanco;
+  document.body.style.backgroundColor = blanco ? "black" : "#FFF5E1";
+  document.body.style.color = blanco ? "black" : "#A67C52";
+  blanco = !blanco;
 }
 
-
 function copyright() {
-alert ("Desarrollado por: Ciro Toledo --- Mail:cirotoledo2009@hotmail.com --- Todos los derechos reservados. ©"
-)
+  alert(
+    "Desarrollado por: Ciro Toledo --- Mail:cirotoledo2009@hotmail.com --- Todos los derechos reservados. ©"
+  );
 }
